c4a7f50e2b70de1f4d7b1ca52faab822f630814f
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-12-08 13:00:23 -0500

    setting up recipe details

--- src/components/RecipesCard.js
+++ src/components/RecipesCard.js
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useFetch } from '../customHooks/useFetch'
 
+import { Modal } from './Modal'
+import { useModal } from '../customHooks/useModal'
+
 import Loader from './Loader'
 
 import './RecipesCard.css'
@@ -13,8 +16,11 @@ function RecipesCard() {
 	const [form, setForm] = useState({})
 	const [recipesBook, setRecipesBook] = useState('own book')
 
+	let extid = ''
+
 	// setRecipeName(form.recipeName)
 	// setCuisine(form.cuisine)
+	const [isOpen, openModal, closeModal] = useModal(false)
 
 	let url = ''
 
@@ -38,6 +44,11 @@ function RecipesCard() {
 		})
 	}
 
+	const handleExtermalId = id => {
+		extid = id
+		console.log('recipe.id', extid.id)
+	}
+
 	if (recipesBook === 'own book') {
 		url = `recipes`
 	} else if (recipesBook === 'external book') {
@@ -57,6 +68,8 @@ function RecipesCard() {
 	}
 
 	const { fetchData, loading } = useFetch(url, token)
+	// console.log(Object.keys(fetchData))
+	// console.log(Object.values(fetchData)[0].id)
 
 	return (
 		<div>
@@ -108,7 +121,11 @@ function RecipesCard() {
 										<h4 className="recipe-servings">servings: {recipe.servings}</h4>
 									) : null}
 									{recipesBook === 'own book' ? null : (
-										<button className="recipe-btn">add to book</button>
+										<button
+											className="recipe-btn"
+											onClick={() => handleExtermalId({ id: recipe.id })}>
+											add to book
+										</button>
 									)}
 									<button className="recipe-btn">add to menu</button>
 								</article>
@@ -117,6 +134,10 @@ function RecipesCard() {
 					)}
 				</div>
 			</section>
+
+			<Modal isOpen={isOpen} closeModal={closeModal}>
+				<h1>Modal</h1>
+			</Modal>
 		</div>
 	)
 }
