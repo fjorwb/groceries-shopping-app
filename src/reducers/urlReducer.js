40bae398cb924dc17e44f085ef01494ae84f96c2
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-01-05 09:31:42 -0500

    include url in global state

---
+++ src/reducers/urlReducer.js
@@ -0,0 +1,10 @@
+const urlInitialStore = {
+	// url: 'https://groceries-shopping.herokuapp.com/'
+	url: 'http://localhost:5000/'
+}
+
+export const urlReducer = (state = urlInitialStore, action) => {
+	return state
+}
+
+export default urlReducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../reducers/authReducer'
+import urlReducer from '../reducers/urlReducer'
 
 export const store = configureStore({
 	reducer: {
-		auth: authReducer
+		auth: authReducer,
+		url: urlReducer
 	}
 })
 
