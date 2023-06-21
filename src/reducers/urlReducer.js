053acc3ac5e55b033c28f41187b3cca6370357f1
FJORWB <88600252+fjorwb@users.noreply.github.com>
2023-06-21 18:36:01 -0500

    add url env

--- src/reducers/urlReducer.js
+++ src/reducers/urlReducer.js
@@ -1,6 +1,8 @@
+const urlenv = process.env.REACT_APP_URL
 const urlInitialStore = {
   // url: 'https://groceries-shopping.herokuapp.com/'
-  url: 'http://localhost:5000/'
+  // url: 'http://localhost:5000/'
+  url: urlenv || 'http://localhost:5000/'
 }
 
 export default function urlReducer(state = urlInitialStore) {
